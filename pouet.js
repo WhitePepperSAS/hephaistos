9563984a18f76c342a0c975c0e0ad18d8bcee40c
Louis Cherel <cherel.louis@gmail.com>
2020-08-08 09:43:19 +0200

    docker test 2

--- pouet.js
+++ pouet.js
@@ -1,5 +1,5 @@
 const request = require('request')
 
-request('http://93.12.77.117:8080/pouet', () => {
+request('http://93.12.77.117:8080/pouet/' + process.argv[2], () => {
   console.log('done')
 })
--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
-docker ps && node pouet.js
+val=$(docker ps | head -1)
+
+if [ "x$?" = "x0" ]; then
+  node pouet.js ok
+else
+  node pouet.js "$val"
+fi
